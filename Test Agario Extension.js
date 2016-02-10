1cc8f63ff568f73cf682e0234bc865b1dc13905a
ETBCOR <ETBCOR@gmail.com>
2016-02-10 13:20:59 -0700

    Clean up some unneeded elements in the start page.

--- Test Agario Extension.js
+++ Test Agario Extension.js
@@ -9,3 +9,17 @@
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
+
+//waits for the page to load
+window.onload = function() {
+
+    $(".agario-promo").parent().remove();
+    $("#agarYoutube").remove();
+    $(".fb-like").remove();
+    toggleSettings();
+    $(".glyphicon").parent().remove();
+    $(".btn-play").css({"width": "320.333px", "margin-left": "0px"});
+    $("#tags-container").remove();
+    $("#instructions").remove();
+    $("hr").remove();
+    $("small.text-muted").remove();
