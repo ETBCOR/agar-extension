59353ab77df2f386f15ba76f343968564d9252d9
ETBCOR <ETBCOR@gmail.com>
2016-02-10 13:17:53 -0700

    Initial Commit

---
+++ Test Agario Extension.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Agar.io Extension Test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Learning to make (agar.io) userscripts!
+// @author       Critical Split
+// @match        http://agar.io/
+// @require      http://pastebin.com/raw/QrMULMQV
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
