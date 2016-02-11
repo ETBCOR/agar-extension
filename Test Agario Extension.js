46e94bf97d15f324feaf73fb2fbd585e7b3a6f5d
ETBCOR <ETBCOR@gmail.com>
2016-02-11 15:49:36 -0700

    Add Gamepad Side-Panel

--- Test Agario Extension.js
+++ Test Agario Extension.js
@@ -45,4 +45,11 @@ window.onload = function() {
 
     //sets show mass to true on extension load
     setShowMass(true);
+	//Add gamepad side-panel.
+	$(".agario-promo").replaceWith('<div class="agario-panel agario-side-panel" style="padding-left: 15px; padding-bottom: 15px; padding-right: 15px;"><h4 style="text-align: center;">Gamepad Mode</h4><button class="btn btn-gamepad">Gamepad Mode</button><button class="btn btn-no-gamepad">Regular Mode</button></div>');
+	$(".btn-gamepad").on('click', function () {window.location = 'http://www.agar.io/?gamepad'});
+	$(".btn-gamepad").css({"width": "190px", "margin-bottom": "7.5px", "color": "#fff", "background-color": "#A901DB", "border-color": "#8904B1"});
+	$(".btn-no-gamepad").on('click', function () {window.location = 'http://www.agar.io/'});
+	$(".btn-no-gamepad").css({"width": "190px", "color": "#fff", "background-color": "#D7DF01", "border-color": "#AEB404"});
+
 }
